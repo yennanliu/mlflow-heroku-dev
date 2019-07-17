49da1082d671dfa5cedc8e1e2ebe62ac692a60b2
yennanliue <f339339@gmail.com>
2019-07-17 22:41:06 +0800

    fix code layout (run_more_demo_projects.sh)

--- run_more_demo_projects.sh
+++ run_more_demo_projects.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 git clone https://github.com/mlflow/mlflow.git 
+
 # run project 1 
 for alpha in 0.3 0.4
 do  
-	echo 'alpha = ' $alpha && mlflow run mlflow/examples/sklearn_elasticnet_wine -P alpha=$alpha; 
+    echo 'alpha = ' $alpha && mlflow run mlflow/examples/sklearn_elasticnet_wine -P alpha=$alpha; 
 done 
-
 # run project 2 
 mlflow run examples/sklearn_logistic_regression 
