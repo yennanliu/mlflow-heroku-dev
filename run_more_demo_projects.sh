b0870c5671fdce572f2ed0e64d1ad3abeaeb3ed2
yennanliue <f339339@gmail.com>
2019-07-17 21:43:08 +0800

    run the demo ML prokect with different parameters

--- run_more_demo_projects.sh
+++ run_more_demo_projects.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-cd && git clone https://github.com/mlflow/mlflow.git && cd mlflow 
-mlflow run examples/sklearn_elasticnet_wine -P alpha=0.4
+cd && cd mlflow-heroku-dev && git clone https://github.com/mlflow/mlflow.git && cd mlflow 
+# run project 1 
+for alpha in 0.2 0.3 0.4 
+do  
+	echo 'alpha = ' $alpha && mlflow run examples/sklearn_elasticnet_wine -P alpha=$alpha; 
+done 
+
+# run project 2 
 mlflow run examples/sklearn_logistic_regression 
