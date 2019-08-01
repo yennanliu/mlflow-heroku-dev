e284f2e263ad185bc01c6dafa9c05d6eb99d7d54
yennanliu <f339339@gmail.com>
2019-08-01 18:19:17 +0800

    fix code layout

--- run_XJob_mlflow_projects.sh
+++ run_XJob_mlflow_projects.sh
@@ -4,17 +4,17 @@
 git clone https://github.com/yennanliu/XJob.git 
 
 run_projects(){ 
-mlflow run XJob/mlflow/sklearn_logistic_regression 
-mlflow run XJob/mlflow/sklearn_elasticnet_diabetes/osx/
-mlflow run XJob/mlflow/sklearn_elasticnet_wine 
+    mlflow run XJob/mlflow/sklearn_logistic_regression 
+    mlflow run XJob/mlflow/sklearn_elasticnet_diabetes/osx/
+    mlflow run XJob/mlflow/sklearn_elasticnet_wine 
 }
 
 run_projects_with_parameters(){ 
 for alpha in 0.3 0.4
 do 
-	echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_logistic_regression  -P alpha=$alpha;
-	echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_elasticnet_diabetes/osx/ -P alpha=$alpha;
-	echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_elasticnet_wine -P alpha=$alpha;
+    echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_logistic_regression  -P alpha=$alpha;
+    echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_elasticnet_diabetes/osx/ -P alpha=$alpha;
+    echo 'alpha = ' $alpha && mlflow run XJob/mlflow/sklearn_elasticnet_wine -P alpha=$alpha;
 done 
 }
 
